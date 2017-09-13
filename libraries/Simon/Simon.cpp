84bdef5794b285e7cc23e0d69918c86e856d149a
Alec Rager <arager@umich.edu>
2017-09-13 19:59:07 -0400

    Commented Simon Code

--- libraries/Simon/Simon.cpp
+++ libraries/Simon/Simon.cpp
@@ -2,90 +2,98 @@
 
 
 Simon::Simon(int p, int rl, int rb, int yl, int yb, 
-			 int gl, int gb, int bl, int bb) :
-piezo(p), level(0), listening(false), verbose(false) {
-	game = new NodeGame(p);
-	game->addNode("Red", rl, rb, NOTE_A4);
-	game->addNode("Yellow", yl, yb, NOTE_CS4);
-	game->addNode("Green", gl, gb, NOTE_E5);
-	game->addNode("Blue", bl, bb, NOTE_E4);
-	randomSeed(analogRead(0));
-	newSequence();
-}
-
-
-Simon::~Simon(){
-	delete game;
-}
-
-void Simon::newSequence(){
-	for (int i = 0; i < numLevels; i++) correctSequence[i] = random(0, 4);
-	level = 1;
-}
-
-void Simon::playSequence(){
-	delay(1000);
-	int duration = 300 - (level * 10);
-	for (int i = 0; i < level; i++){
-		game->getNode(correctSequence[i])->play(duration);
-		delay(duration/3);
-	}
-}
-
-void Simon::printSequence(){
-	Serial.print("[ ");
-	for (int i = 0; i < level; i++){
-		Serial.print(game->getNode(correctSequence[i])->name);
-		Serial.print(" ");
-	}
-	Serial.print("]");
-}
-
-void Simon::printGame(){
-	printSequence();
-	Serial.print("\t");
-	game->print();
-	Serial.println();
-}
-
-void Simon::playIncorrect(){
-	tone(piezo, NOTE_E2);
-	for(int i = 0; i < 100; i++){
-		game->getNode(random(0, 4))->light(10);
-	}
-	noTone(piezo);
-}
-
-void Simon::testing(){
-	Serial.begin(9600);
-	verbose = true;
+             int gl, int gb, int bl, int bb)
+    : piezo(p), level(0), listening(false), verbose(false) {
+  game = new NodeGame(p);
+  game->addNode("Red", rl, rb, NOTE_A4);
+  game->addNode("Yellow", yl, yb, NOTE_CS4);
+  game->addNode("Green", gl, gb, NOTE_E5);
+  game->addNode("Blue", bl, bb, NOTE_E4);
+  randomSeed(analogRead(2));
+  newSequence();
 }
 
 void Simon::play(){
-	if (listening){
-		if (game->read() and verbose) printGame();
-		if (game->compare(correctSequence, level)){
-			if (game->match(correctSequence, level)){
-				if (level == numLevels) newSequence();
-				else level++;
-				listening = false;
-				game->clear();
-			}
-		}
-		else{
-			playIncorrect();
-			newSequence();
-			listening = false;
-			game->clear();
-			if (verbose) printGame();
-		}
-	}
-	else{
-		if (verbose) printGame();
-		playSequence();
-		listening = true;
-	}
-	
+  if (listening) {
+    // Call NodeGame's read()
+    if (game->read() and verbose) printGame();
+    // If input could eventually match correct sequence
+    if (game->compare(correctSequence, level)) {
+      // if input does match correct sequence
+      if (game->match(correctSequence, level)) {
+        // Increment level and start new round
+        if (level == numLevels) newSequence();
+        else level++;
+        listening = false;
+        game->clear();
+      }
+    }
+    // If input can no longer equal correct sequence
+    else{
+      // Signal incorrect and begin new game
+      playIncorrect();
+      newSequence();
+      listening = false;
+      game->clear();
+      if (verbose) printGame();
+    }
+  }
+  // If not accepting button input
+  else{
+    // Output next corrent sequence
+    if (verbose) printGame();
+    playSequence();
+    listening = true;
+  }
 }
+
+void Simon::newSequence(){
+  for (int i = 0; i < numLevels; i++) correctSequence[i] = random(0, 4);
+  level = 1;
+}
+
+void Simon::playSequence(){
+  delay(1000);
+  int duration = 300 - (level * 10);
+  for (int i = 0; i < level; i++){
+    game->getNode(correctSequence[i])->play(duration);
+    delay(duration/3);
+  }
+}
+
+void Simon::playIncorrect(){
+  tone(piezo, NOTE_E2);
+  for(int i = 0; i < 100; i++){
+    game->getNode(random(0, 4))->light(10);
+  }
+  noTone(piezo);
+}
+
+void Simon::testing(){
+  Serial.begin(9600);
+  verbose = true;
+}
+
+void Simon::printSequence(){
+  Serial.print("[ ");
+  for (int i = 0; i < level; i++){
+    Serial.print(game->getNode(correctSequence[i])->name);
+    Serial.print(" ");
+  }
+  Serial.print("]");
+}
+
+void Simon::printGame(){
+  printSequence();
+  Serial.print("\t");
+  game->print();
+  Serial.println();
+}
+
+
+
+
+
+
 
 
--- libraries/Simon/Simon.h
+++ libraries/Simon/Simon.h
@@ -3,41 +3,67 @@
 
 #include "../NodeGame/NodeGame.h"
 
+// Maximum number of levels. Matched with 
+// NodeGame's buffer size.
 const int numLevels = 128;
 
 
-
+// Plays Simon Says with 4 nodes. 
 class Simon {
+  public:
+    // Set up a NodeGame with (piezo pin,
+    // red LED, red button, yellow LED,
+    // yellow button, green LED, green 
+    // button, blue LED, blue button).
+    Simon(int p, int rl, int rb, 
+          int yl, int yb, int gl, 
+          int gb, int bl, int bb);
+    ~Simon() { delete game; }
 
-private:
+    // Either accept button input or display 
+    // the correct sequence, depending on 
+    // listening state. Increases in speed 
+    // as game progresses. Clears game with 
+    // incorrect input
+    void play();
 
-	NodeGame *game;
+    // Creates a new random sequence to compare
+    // to NodeGame's recording buffer.
+    void newSequence();
+    // Plays the correct sequence up to the 
+    // current level. 
+    void playSequence();
 
-	int piezo;
-	int correctSequence[numLevels];
-	bool listening;
-	bool verbose;
+    // Signal user that they were incorrect.
+    void playIncorrect();
 
-public:
+    // Sets the verbose boolean to true, enabling
+    // print statements throughout the program.
+    void testing();
+    // Prints randomized sequence through current
+    // level.
+    void printSequence();
+    // Prints sequence and NodeGame's buffer.
+    void printGame();
 
-	int level;
+  private:
+    // Pointer to dynamically allocated NodeGame
+    NodeGame *game;
 
-	Simon(int p, int rl, int rb, 
-		  int yl, int yb, int gl, 
-		  int gb, int bl, int bb);		
-	~Simon();
+    // Current level. Initialized to 1
+    int level;
 
-	void play();
+    // Stores correct sequence for entire game.
+    int correctSequence[numLevels];
 
-	void newSequence();
-	void playSequence();
+    // Allows button input or sequence output
+    bool listening;
 
-	void playIncorrect();
+    // Allows print statements
+    bool verbose;
 
-	void testing();
-	void printSequence();
-	void printGame();
-
+    // Noisemaker location
+    int piezo;
 };
 
 #endif
